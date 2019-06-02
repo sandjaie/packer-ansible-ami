b3c6586fe5a11c2f75f233f805a50563e1bf924c
sandjaie <sandjaie@gmail.com>
2019-06-03 00:56:57 +0530

    fixed errors

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -eux
 #remove ansible
 
-pip3 uninstall ansible
+#pip3 uninstall ansible
+
+#apt
+# Uninstall Ansible and remove PPA.
+sudo apt -y remove --purge ansible
+sudo apt-add-repository --remove ppa:ansible/ansible
+
+# Apt cleanup.
+sudo apt -y autoremove
+sudo apt -y update
--- packer/scripts/install_ansible.sh
+++ packer/scripts/install_ansible.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -eux
 
 # Install Ansible repository.
-yum -y install python3
-pip3 install ansible
+# amazon linux
+# sudo yum -y install python3
+# sudo pip3 install ansible
+
+#apt
+sudo apt -y update
+sudo apt -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+
+# Install Ansible.
+sudo apt -y update
+sudo apt -y install ansible
